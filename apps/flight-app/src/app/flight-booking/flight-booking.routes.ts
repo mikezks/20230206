80c6b9352132a0623598fbd913613a711a6ed85a
mikezks <23002661+mikezks@users.noreply.github.com>
2023-02-08 16:50:54 +0100

    feat: feature routes

--- apps/flight-app/src/app/flight-booking/flight-booking.routes.ts
+++ apps/flight-app/src/app/flight-booking/flight-booking.routes.ts
@@ -4,23 +4,30 @@ import { Routes } from '@angular/router';
 import { FlightSearchComponent } from './flight-search/flight-search.component';
 import { PassengerSearchComponent } from './passenger-search/passenger-search.component';
 import { FlightEditComponent } from './flight-edit/flight-edit.component';
+import { FlightBookingComponent } from './flight-booking.component';
 
 export const FLIGHT_BOOKING_ROUTES: Routes = [
   {
-    path: '',
-    redirectTo: 'flight-search',
-    pathMatch: 'full',
-  },
-  {
-    path: 'flight-search',
-    component: FlightSearchComponent,
-  },
-  {
-    path: 'passenger-search',
-    component: PassengerSearchComponent,
-  },
-  {
-    path: 'flight-edit/:id',
-    component: FlightEditComponent,
+    path: 'flight-booking',
+    component: FlightBookingComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'flight-search',
+        pathMatch: 'full',
+      },
+      {
+        path: 'flight-search',
+        component: FlightSearchComponent,
+      },
+      {
+        path: 'passenger-search',
+        component: PassengerSearchComponent,
+      },
+      {
+        path: 'flight-edit/:id',
+        component: FlightEditComponent,
+      },
+    ],
   },
 ];
